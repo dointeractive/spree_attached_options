ff21e8c6f72c4a56d8823f35ebea37eae6065eba
Oleg Litvin <oleg.litvin@live.ru>
2013-08-02 01:00:30 +0400

    Added routes for attached_option_types

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
+  namespace :admin do
+    resources :products do
+      resources :attached_option_types
+    end
+  end
 end
