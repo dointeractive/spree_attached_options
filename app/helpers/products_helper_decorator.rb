6bcb9dacb4af498b3429f62a6b91ff9a862e68de
Oleg Litvin <oleg.litvin@live.ru>
2013-08-05 02:05:07 +0400

    Extract code for attached_option_type select to a helper

---
+++ app/helpers/products_helper_decorator.rb
@@ -0,0 +1,8 @@
+module Spree
+  ProductsHelper.class_eval do
+    def select_tag_for_attached_option_type(attached_option_type)
+      option_values = attached_option_type.option_values.map{ |ov| [ ov.presentation, ov.id ] }
+      select_tag :attached_option_value, options_for_select(option_values) 
+    end
+  end
+end
