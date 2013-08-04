99148d879cffd924491eddf490af4e922cdceb11
Oleg Litvin <oleg.litvin@live.ru>
2013-08-04 17:43:00 +0400

    Renamed an override file

--- app/overrides/admin/attached_option_types_in_product_sub_menu.rb
+++ app/overrides/admin/attached_option_types_in_product_sub_menu.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path: "spree/admin/shared/_product_tabs",
-                     name: "ad_hoc_options_in_product_tabs",
+                     name: "attached_option_types_in_product_tabs",
                      insert_bottom: "[data-hook='admin_product_tabs']",
                      erb: %q[ 
                         <li <%= ' class=active' if current == 'Ad Hoc Option Types' %>>
