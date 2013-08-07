3cd078047f78b1b343deaea21873930ae9b1bc06
Oleg Litvin <oleg.litvin@live.ru>
2013-08-07 18:29:00 +0400

    Added options to the select_tag_for_attached_option_type helper

--- app/helpers/products_helper_decorator.rb
+++ app/helpers/products_helper_decorator.rb
@@ -1,8 +1,8 @@
 module Spree
   ProductsHelper.class_eval do
-    def select_tag_for_attached_option_type(attached_option_type)
+    def select_tag_for_attached_option_type(attached_option_type, options={})
       option_values = attached_option_type.option_values.map{ |ov| [ ov.presentation, ov.id ] }
-      select_tag "products[#{@product.master.id}][option_value_ids][]", options_for_select(option_values) 
+      select_tag "products[#{@product.master.id}][option_value_ids][]", options_for_select(option_values), options
     end
   end
 end
