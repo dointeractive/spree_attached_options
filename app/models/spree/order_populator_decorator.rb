068dfabc4676b4a7a20b301375f96d6aaa840f0c
Oleg Litvin <oleg.litvin@live.ru>
2013-08-07 20:44:28 +0400

    Extracted methods for quantity and option_value_ids

--- app/models/spree/order_populator_decorator.rb
+++ app/models/spree/order_populator_decorator.rb
@@ -1,23 +1,28 @@
 ﻿module Spree
   OrderPopulator.class_eval do
     def populate(from_hash)
-      from_hash[:products].each do |product_id, attrs|
-        variant = get_variant(attrs[:variant])
-        option_value_ids = attrs[:option_value_ids]
-        quantity = from_hash[:quantity]
-        attempt_cart_add(variant, option_value_ids, quantity)
+      @from_hash = from_hash
+      @from_hash[:products].each do |product_id, attrs|
+        attempt_cart_add(product_id)
       end
       valid?
     end
 
     private 
 
-      def get_variant(variant_attrs)
-        Spree::Variant.find(variant_attrs[:id])
+      def get_variant(product_id)
+        Spree::Variant.find(@from_hash[:products][product_id][:variant][:id])
+      end
+      
+      def get_option_value_ids(product_id)
+        @from_hash[:products][product_id][:option_value_ids]
       end
 
-      def attempt_cart_add(variant, option_value_ids, quantity)
-        quantity = quantity.to_i
+      def quantity
+        @from_hash[:quantity].to_i
+      end
+
+      def attempt_cart_add(product_id)
         # 2,147,483,647 is crazy.
         # See issue #2695.
         if quantity > 2_147_483_647
@@ -25,8 +30,11 @@
           return false
         end
 
+        variant = get_variant(product_id)
+
         if quantity > 0
           if check_stock_levels(variant, quantity)
+            option_value_ids = get_option_value_ids(product_id)
             if option_value_ids
               @order.contents.add_with_option_values(variant, option_value_ids, quantity, currency)
             else
