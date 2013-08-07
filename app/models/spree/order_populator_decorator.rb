e9e8a99c891c767f8c5259d87b00d68ce8653986
Oleg Litvin <oleg.litvin@live.ru>
2013-08-07 19:49:45 +0400

    Extracted variant getter to add more flexibility

--- app/models/spree/order_populator_decorator.rb
+++ app/models/spree/order_populator_decorator.rb
@@ -2,30 +2,38 @@ module Spree
   OrderPopulator.class_eval do
     def populate(from_hash)
       from_hash[:products].each do |product_id, attrs|
-        attempt_cart_add(attrs[:variant_id], from_hash[:quantity], attrs[:option_value_ids])
+        variant = get_variant(attrs[:variant])
+        option_value_ids = attrs[:option_value_ids]
+        quantity = from_hash[:quantity]
+        attempt_cart_add(variant, option_value_ids, quantity)
       end
       valid?
     end
 
-    def attempt_cart_add(variant_id, quantity, option_value_ids)
-      quantity = quantity.to_i
-      # 2,147,483,647 is crazy.
-      # See issue #2695.
-      if quantity > 2_147_483_647
-        errors.add(:base, Spree.t(:please_enter_reasonable_quantity, :scope => :order_populator))
-        return false
+    private 
+
+      def get_variant(variant_attrs)
+        Spree::Variant.find(variant_attrs(id))
       end
 
-      variant = Spree::Variant.find(variant_id)
-      if quantity > 0
-        if check_stock_levels(variant, quantity)
-          if option_value_ids
-            @order.contents.add_with_option_values(variant, option_value_ids, quantity, currency)
-          else
-            @order.contents.add(variant, quantity, currency)
+      def attempt_cart_add(variant_id, option_value_ids, quantity)
+        quantity = quantity.to_i
+        # 2,147,483,647 is crazy.
+        # See issue #2695.
+        if quantity > 2_147_483_647
+          errors.add(:base, Spree.t(:please_enter_reasonable_quantity, :scope => :order_populator))
+          return false
+        end
+
+        if quantity > 0
+          if check_stock_levels(variant, quantity)
+            if option_value_ids
+              @order.contents.add_with_option_values(variant, option_value_ids, quantity, currency)
+            else
+              @order.contents.add(variant, quantity, currency)
+            end
           end
         end
       end
-    end
   end # end OrderPopulator
 end
