2bba371d46d1900bd787d889703f5cd89ee7ff29
Oleg Litvin <oleg.litvin@live.ru>
2013-08-07 19:48:41 +0400

    Fixed copy-paste bug in products helper

--- app/helpers/products_helper_decorator.rb
+++ app/helpers/products_helper_decorator.rb
@@ -2,7 +2,7 @@ module Spree
   ProductsHelper.class_eval do
     def select_tag_for_attached_option_type(attached_option_type, options={})
       option_values = attached_option_type.option_values.map{ |ov| [ ov.presentation, ov.id ] }
-      select_tag "products[#{@product.master.id}][option_value_ids][]", options_for_select(option_values), options
+      select_tag "products[#{@product.id}][option_value_ids][]", options_for_select(option_values), options
     end
   end
 end
