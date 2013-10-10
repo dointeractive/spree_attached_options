595323b0c95bd97de608a738f954412f37d29dd0
Oleg Litvin <oleg.litvin@live.ru>
2013-10-11 03:03:19 +0400

    Get rid of a junk in the install generator

--- lib/generators/spree_attached_options/install/install_generator.rb
+++ lib/generators/spree_attached_options/install/install_generator.rb
@@ -5,13 +5,7 @@ module SpreeAttachedOptions
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_attached_options\n"
         append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_attached_options\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_attached_options\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_attached_options\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
