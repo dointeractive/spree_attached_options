dc79edd0d7f057cec42efc57e6dcac10ce3ff7f1
Oleg Litvin <oleg.litvin@live.ru>
2013-10-11 02:37:10 +0400

    Added attached options to the product show

--- app/overrides/store/attached_option_types_at_products_show.rb
+++ app/overrides/store/attached_option_types_at_products_show.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(virtual_path: 'spree/products/_cart_form',
                      name: 'attached_option_types_at_products_show',
-                     insert_after: '[data-hook="inside_product_cart_form"]',
+                     insert_before: '[data-hook="inside_product_cart_form"]',
                      partial: 'spree/products/attached_option_types' )
