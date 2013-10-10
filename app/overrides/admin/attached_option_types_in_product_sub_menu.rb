91a35cec500a9bb527f78edc98e315f5b4ecbfe7
Oleg Litvin <oleg.litvin@live.ru>
2013-10-11 01:37:45 +0400

    Added icon for product tabs menu

--- app/overrides/admin/attached_option_types_in_product_sub_menu.rb
+++ app/overrides/admin/attached_option_types_in_product_sub_menu.rb
@@ -3,6 +3,6 @@ Deface::Override.new(virtual_path: "spree/admin/shared/_product_tabs",
                      insert_bottom: "[data-hook='admin_product_tabs']",
                      erb: %q[ 
                         <li <%= ' class=active' if current == 'Attached Options' %>>
-                          <%= link_to Spree.t(:attached_options), admin_product_attached_options_path(@product) %>
+                          <%= link_to_with_icon 'icon-paper-clip', Spree.t(:attached_options), admin_product_attached_options_path(@product) %>
                         </li>
                      ])
