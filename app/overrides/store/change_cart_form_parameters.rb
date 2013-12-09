9e8c7b433ba4c0961966d70991a5e2ec64165d02
Oleg Litvin <oleg.litvin@live.ru>
2013-12-09 16:29:10 +0400

    Fixed overrides for new deface syntax and _shipment_manifest partial

--- app/overrides/store/change_cart_form_parameters.rb
+++ app/overrides/store/change_cart_form_parameters.rb
@@ -7,14 +7,14 @@ Deface::Override.new(virtual_path: 'spree/products/_cart_form',
 
 Deface::Override.new(virtual_path: 'spree/products/_cart_form',
                      name: 'change_variant_id_parameter',
-                     replace: 'code:contains(\'radio_button_tag "products[#{@product.id}]"\')',
+                     replace: 'erb:contains(\'radio_button_tag "products[#{@product.id}]"\')',
                      erb: %q[ 
                       <%= radio_button_tag "products[#{@product.id}][variant][id]", variant.id, index == 0, 'data-price' => variant.price_in(current_currency).display_price %> 
                      ])
 
 Deface::Override.new(virtual_path: 'spree/products/_cart_form',
                      name: 'change_quantity_parameter',
-                     replace: 'code:contains("number_field_tag (@product.variants_and_option_values.any?")',
+                     replace: 'erb:contains("number_field_tag (@product.variants_and_option_values.any?")',
                      erb: %q[ 
                        <%= number_field_tag :quantity, 1, :class => 'title', :min => 1 %>
                      ])
