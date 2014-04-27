21a9d05e2b5bae0ee870c60ca8d8b16d3e04298a
Oleg Litvin <oleg.litvin@live.ru>
2014-04-28 03:18:26 +0400

    Fixed issues with attr_accessors and strong_parameters

--- app/controllers/spree/admin/attached_options_controller.rb
+++ app/controllers/spree/admin/attached_options_controller.rb
@@ -1,9 +1,9 @@
 module Spree
   class Admin::AttachedOptionsController < Admin::BaseController
-    before_filter :load_product, except: [:destroy]
+    before_filter :load_product, except: :destroy
 
     def create
-      unless @product.master.update_attributes params[:master]
+      unless @product.master.update_attributes create_params
         flash[:error] = Spree.t :fail_to_create_attached_options
       end
       redirect_to admin_product_attached_options_path
@@ -12,6 +12,7 @@ module Spree
     def destroy
       @attached_option = Spree::AttachedOption.find params[:id]
       @attached_option.destroy
+
       respond_with(@attached_option) do |format|
         format.html { redirect_to admin_product_attached_options_path }
         format.js   { render :partial => "spree/admin/shared/destroy" }
@@ -20,9 +21,13 @@ module Spree
 
     private
 
+    def create_params
+      params.require(:master).permit(attached_options_attributes: [:option_value_id])
+    end
+
     def load_product
       @product = Spree::Product
-        .where(permalink: params[:product_id])
+        .where(slug: params[:product_id])
         .includes(master: :attached_options)
         .first
     end
