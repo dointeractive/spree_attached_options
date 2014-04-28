6bee0a47181e2c949307423cc234d8563ca2bec3
Oleg Litvin <oleg.litvin@live.ru>
2014-04-28 19:52:07 +0400

    Fixed admin order/edit/_line_items view

--- app/overrides/admin/attached_options_in_order_shipments.rb
+++ app/overrides/admin/attached_options_in_order_shipments.rb
@@ -2,5 +2,6 @@ Deface::Override.new(virtual_path: 'spree/admin/orders/_shipment_manifest',
                      name: 'attached_options_in_order_shipments',
                      replace: 'erb:contains("variant_options(item.variant)")',
                      erb: %q[
-                       <%= line_item_name(item.line_item) %><br>
+                       <%= text = line_item_name(item.line_item) %>
+                       <%= raw('<br>') if text %>
                      ])
